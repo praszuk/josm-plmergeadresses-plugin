b2aa899aba90cedf9f259913bf79aa62791b6c37
Paweł Raszuk <pawel.raszuk@gmail.com>
2024-02-10 15:13:16 +0100

    Gradle and plugin init

---
+++ src/main/java/org/openstreetmap/josm/plugins/plmergeaddresses/MergeAddressesAction.java
@@ -0,0 +1,13 @@
+package org.openstreetmap.josm.plugins.plmergeaddresses;
+
+import org.openstreetmap.josm.actions.JosmAction;
+
+import java.awt.event.ActionEvent;
+
+public class MergeAddressesAction extends JosmAction {
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+}
---
+++ src/main/java/org/openstreetmap/josm/plugins/plmergeaddresses/MergeAddressesPlugin.java
@@ -0,0 +1,13 @@
+package org.openstreetmap.josm.plugins.plmergeaddresses;
+
+import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.gui.MainMenu;
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class MergeAddressesPlugin extends Plugin {
+    public MergeAddressesPlugin(PluginInformation info){
+        super(info);
+        MainMenu.add(MainApplication.getMenu().selectionMenu, new MergeAddressesAction());
+    }
+}
