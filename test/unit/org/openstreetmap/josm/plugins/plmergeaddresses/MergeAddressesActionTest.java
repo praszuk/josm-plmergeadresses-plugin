53e0a439bc45369a861a42db37323471672fea22
Paweł Raszuk <pawel.raszuk@gmail.com>
2024-02-10 23:44:13 +0100

    Add test UpdateAddressWithStreetToStreet

--- test/unit/org/openstreetmap/josm/plugins/plmergeaddresses/MergeAddressesActionTest.java
+++ test/unit/org/openstreetmap/josm/plugins/plmergeaddresses/MergeAddressesActionTest.java
@@ -17,6 +17,7 @@ import org.openstreetmap.josm.testutils.JOSMTestRules;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertTrue;
 
@@ -65,7 +66,7 @@ public class MergeAddressesActionTest {
 
         dataSet.setSelected(src, dist);
 
-        UndoRedoHandler.getInstance().add(new SequenceCommand("",new MergeAddressesAction().performMerge(dataSet)));
+        new MergeAddressesAction().performMerge(dataSet);
 
         TagMap expectedTagMap = new TagMap();
         expectedTagMap.putAll(
@@ -80,6 +81,66 @@ public class MergeAddressesActionTest {
                     "source:addr", "gugik.gov.pl"
             )
         );
+        assertTrue(expectedTagMap.getTags().containsAll(dist.getKeys().getTags()));
+    }
+
+    @Test
+    public void testUpdateAddressWithStreetToStreet() {
+        new MockUp<MergeAddressesCommand>(){
+            @Mock
+            boolean mergeTagsAndResolveConflicts(OsmPrimitive dist, OsmPrimitive src){
+                new ChangePropertyCommand(
+                        src.getDataSet(),
+                        Collections.singletonList(dist),
+                        src.getKeys()
+                ).executeCommand();
+                return true;
+            }
+        };
+        ExpertToggleAction.getInstance().setExpert(true);
+
+        DataSet dataSet = new DataSet();
+        OsmPrimitive src = new Node(new LatLon(52.23, 21.01124));
+        OsmPrimitive dist = new Node(new LatLon(52.23, 21.01123));
+        dist.setOsmId(1, 1);
+
+        src.putAll(Map.of(
+                "addr:city:simc", "12345",
+                "addr:city", "Place",
+                "addr:street", "Street2",
+                "addr:housenumber", "1",
+                "addr:postcode", "00-000",
+                "source:addr", "gugik.gov.pl"
+        ));
+        dist.putAll(Map.of(
+                "addr:city:simc", "12345",
+                "addr:street", "Street1",
+                "addr:housenumber", "43A",
+                "addr:postcode", "00-000",
+                "source:addr", "gmina.e-mapa.net"
+        ));
+        dataSet.addPrimitive(src);
+        dataSet.addPrimitive(dist);
+
+        dataSet.setSelected(src, dist);
+
+        new MergeAddressesAction().performMerge(dataSet);
+
+        TagMap expectedTagMap = new TagMap();
+        expectedTagMap.putAll(
+                Map.of(
+                        "addr:city:simc", "12345",
+                        "addr:city", "Place",
+                        "addr:street", "Street2",
+                        "addr:housenumber", "1",
+                        "addr:postcode", "00-000",
+                        "old_addr:street", "Street1",
+                        "old_addr:housenumber", "43A",
+                        "source:addr", "gugik.gov.pl"
+                )
+        );
+        System.out.println(expectedTagMap.getTags());
+        System.out.println(dist.getKeys().getTags());
         assertTrue(expectedTagMap.getTags().containsAll(dist.getKeys().getTags()));
     }
 }
