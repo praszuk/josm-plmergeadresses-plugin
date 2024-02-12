b238ed41584ee143a12928609e2950289a7003ca
Paweł Raszuk <pawel.raszuk@gmail.com>
2024-02-12 23:16:29 +0100

    Add missing test: PlaceToStreetSameHouseNumber

--- test/integration/org/openstreetmap/josm/plugins/plmergeaddresses/MergeAddressesActionTest.java
+++ test/integration/org/openstreetmap/josm/plugins/plmergeaddresses/MergeAddressesActionTest.java
@@ -152,6 +152,44 @@ public class MergeAddressesActionTest {
     }
 
     @Test
+    public void testUpdatePlaceToStreetSameHouseNumber() {
+        ExpertToggleAction.getInstance().setExpert(true);  // avoid asking about merging obvious tags
+        Map.of(
+                "addr:city:simc", "12345",
+                "addr:city", "Place",
+                "addr:street", "Street",
+                "addr:housenumber", "1",
+                "addr:postcode", "00-000",
+                "source:addr", "gugik.gov.pl"
+        ).forEach(newAddress::put);
+        Map.of(
+                "addr:city:simc", "12345",
+                "addr:place", "Place",
+                "addr:housenumber", "1",
+                "addr:postcode", "00-000",
+                "source:addr", "gmina.e-mapa.net"
+        ).forEach(currentAddress::put);
+
+        dataSet.setSelected(newAddress, currentAddress);
+        new MergeAddressesAction().actionPerformed(null);
+
+        TagMap expectedTagMap = new TagMap();
+        expectedTagMap.putAll(
+                Map.of(
+                        "addr:city:simc", "12345",
+                        "addr:city", "Place",
+                        "addr:street", "Street",
+                        "addr:housenumber", "1",
+                        "addr:postcode", "00-000",
+                        "old_addr:place", "Place",
+                        "source:addr", "gugik.gov.pl"
+                )
+        );
+        assertNotNull(UndoRedoHandler.getInstance().getLastCommand());
+        assertTagListEquals(expectedTagMap.getTags(), currentAddress.getKeys().getTags());
+    }
+
+    @Test
     public void testUpdateStreetToNewStreetNewHouseNumber() {
         ExpertToggleAction.getInstance().setExpert(true);  // avoid asking about merging obvious tags
         Map.of(
